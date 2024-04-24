90ec394a392603e17c7f5ac6a177a76ac3664ff3
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-04-24 17:07:00 -0500

    Improve design.

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,4 +1,5 @@
-<footer class="flex justify-between items-center w-screen text-lg px-10 lg:px-[12%] py-3">
-    <p class="font-bold">Cullman Bowling Center</p>
-    <p>Developed by Landon Redmond</p>
+<footer class="flex justify-between items-center w-screen text-lg px-28 py-3">
+    <p>©Copyright 2024 <span class="font-bold">Cullman Bowling Center</span>. All Rights Reserved.</p>
+
+    <p>Developed by <a class="font-bold" href="https://github.com/Lanred-Dev">Landon Redmond</a></p>
 </footer>
