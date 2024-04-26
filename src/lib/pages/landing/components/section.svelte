bc1ba96099a1f39f640d03d7366dae5833158c13
Landon Redmond <landon.redmond0@gmail.com>
2024-04-26 11:29:34 -0500

    Work on section graphics.

--- src/lib/pages/landing/components/section.svelte
+++ src/lib/pages/landing/components/section.svelte
@@ -7,21 +7,28 @@
 
 <span {id} class="relative" />
 
-<div class="flex h-[80vh] w-screen flex-col items-start justify-center px-[16%] text-left">
-    <h1 class="text-5xl font-bold">{@html title}</h1>
-    <p class="mt-5 max-w-[50%] text-lg">{description}</p>
+<div class="section relative flex h-fit min-h-screen w-full flex-col items-center justify-center px-5 sm:px-10 lg:px-0 lg:h-screen lg:flex-row lg:justify-between gap-2" id="section-{id}">
+    <div class="flex h-full w-full flex-col items-center justify-center text-center lg:w-[50%] lg:items-start lg:pl-[16%] lg:text-left">
+        <h1 class="text-5xl font-bold">{@html title}</h1>
+        <p class="mt-5 text-lg">{description}</p>
 
-    <div class="mt-10 flex gap-2">
-        {#each actions as action}
-            <a class="{action.type} flex items-center gap-0.5" href={action.url}>
-                {action.text}
+        <div class="mt-10 flex gap-2">
+            {#each actions as action}
+                <a class="{action.type} flex items-center gap-0.5" href={action.url}>
+                    {action.text}
 
-                {#if action.type === "secondary"}
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="#212121" viewBox="0 0 24 24" stroke-width="1" stroke="#212121" class="aspect-square h-[1.15rem] pt-0.5">
-                        <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
-                    </svg>
-                {/if}
-            </a>
-        {/each}
+                    {#if action.type === "secondary"}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="#212121" viewBox="0 0 24 24" stroke-width="1" stroke="#212121" class="aspect-square h-[1.15rem] pt-0.5">
+                            <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+                        </svg>
+                    {/if}
+                </a>
+            {/each}
+        </div>
+    </div>
+
+    <div class="relative h-fit w-full lg:h-full lg:w-auto lg:flex-grow">
+        <slot />
     </div>
 </div>
+
--- src/lib/pages/landing/parties.svelte
+++ src/lib/pages/landing/parties.svelte
@@ -10,7 +10,7 @@
         {
             text: "Book now!",
             type: "primary",
-            url: "/",
+            url: "/contact",
         },
         {
             text: "Learn more",
@@ -18,4 +18,102 @@
             url: "/birthdays",
         },
     ]}
-/>
+>
+    <div class="flex justify-center items-end">
+        {#each Array(3) as _value, index}
+            <div class="present" style="--color: #f75d4c;" data-id={index}>
+                <div class="lid" />
+                <div class="box" />
+
+                <div class="bow">
+                    {#each Array(3) as _value}
+                        <div />
+                    {/each}
+                </div>
+
+                <div class="ribbon" />
+            </div>
+        {/each}
+    </div>
+</Section>
+
+<style lang="postcss">
+    .present {
+        width: 120px;
+        height: 120px;
+        @apply relative flex flex-col items-center scale-150;
+    }
+
+    .present[data-id="0"] {
+        right: -20px;
+        width: 120px;
+        height: 130px;
+    }
+
+    .present[data-id="1"] {
+        width: 160px;
+        height: 180px;
+    }
+
+    .present[data-id="2"] {
+        left: -20px;
+        width: 100px;
+        height: 110px;
+    }
+
+    .present .lid {
+        width: calc(100% + 10px);
+        @apply z-[2] h-1/5 rounded-md border-4 border-black bg-[--color];
+    }
+
+    .present .box {
+        background-position:
+            0px 0px,
+            20px 20px;
+        background-size: 40px 40px;
+        box-shadow: inset 0px 6px rgba(0, 0, 0, 0.3);
+        @apply z-[1] h-full w-full rounded-b-md border-4 border-t-0 border-black bg-[--color];
+    }
+
+    .present .ribbon {
+        top: -4px;
+        width: 30px;
+        height: calc(100% + 4px);
+        @apply absolute z-[3] rounded-md border-4 border-black bg-[--color];
+    }
+
+    .present .ribbon::before {
+        content: "";
+        height: 5px;
+        top: 21%;
+        @apply absolute block w-full bg-black bg-opacity-30;
+    }
+
+    .present .bow {
+        top: -20px;
+        height: 35px;
+        @apply absolute flex w-full items-end justify-center;
+    }
+
+    .present .bow div {
+        @apply relative aspect-square rounded-full border-4 border-black bg-[--color];
+    }
+
+    .present .bow div:nth-child(1),
+    .present .bow div:nth-child(3) {
+        @apply h-full;
+    }
+
+    .present .bow div:nth-child(2) {
+        bottom: 8px;
+        @apply z-[1] h-2/3;
+    }
+
+    .present .bow div:nth-child(1) {
+        right: -8px;
+    }
+
+    .present .bow div:nth-child(3) {
+        left: -8px;
+    }
+</style>
