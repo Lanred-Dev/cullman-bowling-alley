81c589670c1e5ba8ce4b16facedfcb76072fedde
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-04-27 18:05:45 -0500

    Improve section graphic.

--- src/lib/pages/landing/leagues.svelte
+++ src/lib/pages/landing/leagues.svelte
@@ -21,7 +21,7 @@
         },
     ]}
 >
-    <div class="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 items-end justify-center">
+    <div class="absolute left-1/2 top-[52.5%] flex -translate-x-1/2 -translate-y-1/2 items-end justify-center">
         {#each Array(3) as _value, index}
             <div class="podium" data-id={index}>
                 <div class="stand" />
@@ -29,6 +29,8 @@
                 <svg class="trophy" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={TROPHY_COLORS[index]}>
                     <path fill-rule="evenodd" d="M5.166 2.621v.858c-1.035.148-2.059.33-3.071.543a.75.75 0 0 0-.584.859 6.753 6.753 0 0 0 6.138 5.6 6.73 6.73 0 0 0 2.743 1.346A6.707 6.707 0 0 1 9.279 15H8.54c-1.036 0-1.875.84-1.875 1.875V19.5h-.75a2.25 2.25 0 0 0-2.25 2.25c0 .414.336.75.75.75h15a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-2.25-2.25h-.75v-2.625c0-1.036-.84-1.875-1.875-1.875h-.739a6.706 6.706 0 0 1-1.112-3.173 6.73 6.73 0 0 0 2.743-1.347 6.753 6.753 0 0 0 6.139-5.6.75.75 0 0 0-.585-.858 47.077 47.077 0 0 0-3.07-.543V2.62a.75.75 0 0 0-.658-.744 49.22 49.22 0 0 0-6.093-.377c-2.063 0-4.096.128-6.093.377a.75.75 0 0 0-.657.744Zm0 2.629c0 1.196.312 2.32.857 3.294A5.266 5.266 0 0 1 3.16 5.337a45.6 45.6 0 0 1 2.006-.343v.256Zm13.5 0v-.256c.674.1 1.343.214 2.006.343a5.265 5.265 0 0 1-2.863 3.207 6.72 6.72 0 0 0 .857-3.294Z" clip-rule="evenodd" />
                 </svg>
+
+                <p class="number">{index === 0 ? "2" : index === 1 ? "1" : "3"}</p>
             </div>
         {/each}
     </div>
@@ -37,28 +39,53 @@
 <style lang="postcss">
     .podium {
         width: 150px;
-        @apply bg-slate-300 relative;
+        @apply relative;
     }
 
     .podium[data-id="0"] {
         height: 200px;
+        box-shadow: inset -6px 6px rgba(129, 129, 129, 0.3);
+        @apply rounded-bl-md;
+    }
+
+    .podium[data-id="0"],
+    .podium[data-id="0"] .stand {
+        @apply bg-[#dadada];
     }
 
     .podium[data-id="1"] {
         height: 280px;
+        box-shadow: inset 0px 6px rgba(129, 129, 129, 0.3);
+        @apply z-[2];
+    }
+
+    .podium[data-id="1"],
+    .podium[data-id="1"] .stand {
+        @apply bg-[#dfdfdf];
     }
 
     .podium[data-id="2"] {
         height: 140px;
+        box-shadow: inset 6px 6px rgba(129, 129, 129, 0.3);
+        @apply rounded-br-md;
+    }
+
+    .podium[data-id="2"],
+    .podium[data-id="2"] .stand {
+        @apply bg-[#cccccc];
     }
 
     .podium .stand {
         width: calc(100% + 15px);
-        @apply z-[1] h-3 absolute bottom-full left-1/2 -translate-x-1/2 bg-slate-300 rounded-md;
+        @apply absolute bottom-full left-1/2 z-[1] h-3 -translate-x-1/2 rounded-md;
     }
 
     .podium .trophy {
         bottom: calc(100% + 1px);
         @apply absolute;
+    }
+
+    .podium .number {
+        @apply absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-7xl font-bold text-primary text-opacity-30;
     }
 </style>
