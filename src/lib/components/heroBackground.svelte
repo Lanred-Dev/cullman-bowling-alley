025ffb0f701989b16da6a3165076e5f4d8dae1ff
Landon Redmond <landon.redmond0@gmail.com>
2024-04-30 11:30:14 -0500

    Add hero background component.

---
+++ src/lib/components/heroBackground.svelte
@@ -0,0 +1,49 @@
+<div class="absolute left-0 top-0 z-[-1] flex h-full w-full">
+    {#each Array(3) as _value}
+        <div class="backgroundCircle absolute h-0 -translate-x-1/2 -translate-y-1/2 rounded-full bg-brand opacity-40" />
+    {/each}
+</div>
+
+<style lang="postcss">
+    .backgroundCircle {
+        filter: blur(300px);
+    }
+
+    .backgroundCircle:nth-child(1) {
+        width: 33%;
+        padding-top: 33%;
+        left: 20%;
+        top: 40%;
+    }
+
+    .backgroundCircle:nth-child(2) {
+        width: 2%;
+        padding-top: 2%;
+        left: 52%;
+        top: 50%;
+    }
+
+    .backgroundCircle:nth-child(3) {
+        width: 42%;
+        padding-top: 27%;
+        left: 80%;
+        top: 50%;
+    }
+
+    @media (max-width: 768px) {
+        .backgroundCircle {
+            filter: blur(150px) brightness(200%);
+        }
+
+        .backgroundCircle:nth-child(1) {
+            top: 12%;
+            width: 40%;
+        }
+
+        .backgroundCircle:nth-child(2) {
+            width: 22%;
+            top: 35%;
+            padding-top: 22%;
+        }
+    }
+</style>
