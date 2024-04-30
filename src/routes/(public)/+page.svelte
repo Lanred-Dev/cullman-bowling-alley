49fd105df1365b341d3508b4aa7dcdbc54adb4c4
Landon Redmond <landon.redmond0@gmail.com>
2024-04-30 11:29:55 -0500

    Move hero component.

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
-    import Hero from "$lib/pages/landing/hero.svelte";
+    import Hero from "$lib/components/infoPage/hero.svelte";
 
     import Parties from "$lib/pages/landing/parties.svelte";
     import Leagues from "$lib/pages/landing/leagues.svelte";
     import Reviews from "$lib/pages/landing/reviews.svelte";
 </script>
 
-<Hero />
+<Hero
+    title="Fun For Everyone<br />Young or Old"
+    description="The Cullman Bowling Center offers upgraded bowling lanes with TV monitors, complemented by a full-scale restaurant, arcade area, and family-friendly activities such as Birthday Parties, Leagues, and Glow Bowling on weekends."
+    actions={[
+        {
+            text: "Contact Us",
+            type: "primary",
+            url: "/contact",
+        },
+        {
+            text: "Our Hours",
+            type: "secondary",
+            url: "/hours-and-rates",
+        },
+    ]}
+/>
 
 <Reviews />
 <Parties />
