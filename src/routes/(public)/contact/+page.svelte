1a98d3cb7140be335eda21847e730b513b35ffc3
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-05-01 07:39:05 -0500

    Improve contact page.

---
+++ src/routes/(public)/contact/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Hero from "$lib/components/infoPage/hero.svelte";
+
+    import Location from "$lib/pages/contact/location.svelte";
+</script>
+
+<Hero title="Contact Us" mini={true} />
+
+<div class="grid grid-cols-1 grid-rows-1">
+    <Location />
+</div>
